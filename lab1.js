ee569d8c409e7350929be4c268af316a1df14cc5
hao <hao@gmail.com>
2025-01-08 13:39:24 +0700

    test1

---
+++ lab1.js
@@ -0,0 +1,16 @@
+var massMark = 78
+var heightMark = 1.69
+var BMIMark = massMark/(heightMark*heightMark)
+document.getElementById("output").innerText = "Hello, this text is from JavaScript!";
+
+var massJohn = 92
+var heightJohn = 1.95
+var BMIJohn = massJohn/(heightJohn*heightJohn)
+
+var markHigherBMI = BMIMark > BMIJohn 
+
+
+if(markHigherBMI ==true)
+    console.log("Mark cao hơn")
+else
+    console.log("Mark thấp hơn") 
